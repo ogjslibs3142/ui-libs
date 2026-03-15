225ec8ac1ae5b337dd7541971796b6c6c0d1962b
ogjslibs3142 <origur18@gmail.com>
2026-03-14 21:28:52 -0400

    Update VisualCode.js

--- VisualCode.js
+++ VisualCode.js
@@ -1,6 +1,6 @@
 /* VisualCode.js
    Class-based teaching UI library
-   Version: 4.4.16  (MessageBox Copy button; SD Step-2 long lines)
+   Version: 4.4.17  (MessageBox Copy button; SD Step-2 long lines)
    Exported global: VisualCode
    This New version includes QuizCode
 */
