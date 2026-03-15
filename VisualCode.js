d45576fc0eb8ab8a0dada72998c0e93855a88421
ogjslibs3142 <origur18@gmail.com>
2026-03-14 21:07:38 -0400

    Update VisualCode.js

--- VisualCode.js
+++ VisualCode.js
@@ -734,7 +734,7 @@ ${text}`;
     MessageBox,
     // wiring
     RewireAll,
-    __version: "4.4.15"
+    __version: "4.4.16"
   });
 
   Object.defineProperty(window, "VisualCode", { value: API, writable: false, configurable: false });
